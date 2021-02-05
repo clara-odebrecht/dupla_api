d6c9bd1a8264e6f9a20ea9355d00a1a54c1b2a9e
Derli Machado <dam.machado1@gmail.com>
2021-02-05 15:06:19 -0300

    Fix: removing test.

--- dao/product_dao.py
+++ dao/product_dao.py
@@ -7,12 +7,3 @@ from dao.base_dao import BaseDao
 class ProductDao(BaseDao):
     def __init__(self):
         super().__init__(Product)
-
-
-dao = ProductDao()
-
-product_model = Product('Name1', 111., 'Des01')
-dao.save(product_model)
-product = dao.read_all()
-for p in product:
-    print(p.name)
