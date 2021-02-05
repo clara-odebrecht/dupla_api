779a44ab142053f6860df184c600400e4aaf9e77
Derli Machado <dam.machado1@gmail.com>
2021-02-05 14:50:28 -0300

    Add: product model.

---
+++ dao/product_dao.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.path.append('.')
+from model.product_model import Product
+from dao.base_dao import BaseDao
+
+
+class ProductDao(BaseDao):
+    def __init__(self):
+        super().__init__(Product)
+
+
+dao = ProductDao()
+
+product_model = Product('Name1', 111., 'Des01')
+dao.save(product_model)
+product = dao.read_all()
+for p in product:
+    print(p.name)
--- utils/validators.py
+++ utils/validators.py
@@ -1,16 +1,21 @@
+import sys
+sys.path.append('.')
 from typing import Type
 
-def validate_type(value: object, type: Type, key: str) -> object:
+
+def validate_type(value: object, type: Type, key: str):
     if not isinstance(value, type):
-        raise TypeError(f'{key.captalize()} must be {type}.')
+        raise TypeError(f"{key.capitalize()} must be {type}.")
     return value
 
-def validate_not_empty(value: object, key: str) -> object:
-    if not value.strip(' '):
-        raise ValueError(f'{key.captalize()} can not be empty.')
+
+def validate_not_empty(value: str, key: str):
+    if not value.strip():
+        raise ValueError(f"{key.capitalize()} can't be empty.")
     return value
 
-def validate_length(value: object, max_length: int, key: str) -> object:
-    if len(value) > max_length:
-        raise ValueError(f'{key.captalize()} can not be bigger than {max_length} characteres.')
+
+def validate_length(value: object, max_len: int, key: str):
+    if len(value) > max_len:
+        raise ValueError(f"{key.capitalize()} can't be bigger than {max_len} characters.")
     return value
