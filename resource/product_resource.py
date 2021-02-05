b5fbb520acaa6fcdc35434cd460e5e762727e317
Clara Baumgarten <clara.odebrecht@olist.com>
2021-02-05 16:31:32 -0300

    Resource: criação base e product

---
+++ resource/product_resource.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('.')
+from flask_restful import fields, marshal_with
+from dao.product_dao import ProductDao
+from models.product import Product
+from resources.base_resource import BaseResource
+
+
+class ProductResource(BaseResource):
+    fields = {
+        "id": fields.Integer,
+        "name": fields.String,
+        "price": fields.Float,
+        "description": fields.String,
+    }
+
+    def __init__(self):
+        self.__dao = ProductDao()
+        self.__model_type = Product
+        super().__init__(self.__dao, self.__model_type)
+
+    @marshal_with(fields)
+    def get_cat(self, id = None):
+        return super().get(id)
+
+    @marshal_with(fields)
+    def post(self):
+        return super().post()
+
+    @marshal_with(fields)
+    def put(self, id):
+        return super().put(id)
+
+    @marshal_with(fields)
+    def delete(self, id):
+        return super().delete(id)
