85d6badd18b9eef37697694d2f57c3c41d17c743
Derli Machado <dam.machado1@gmail.com>
2021-02-05 14:46:33 -0300

    Add: base dao.

---
+++ dao/base_dao.py
@@ -0,0 +1,35 @@
+import sys
+
+sys.path.append('.')
+from model.base_model import BaseModel
+from dao.session import Session
+
+
+class BaseDao:
+    def __init__(self, type_model):
+        self.__type_model = type_model
+
+    def save(self, model: BaseModel) -> BaseModel:
+        with Session() as session:
+            session.add(model)
+            session.commit()
+            session.refresh(model)
+            return model
+
+    def read_all(self) -> list:
+        with Session() as session:
+            result = session.query(self.__type_model).order_by('id').all()
+        return result
+
+    def read_by_id(self, id: int) -> BaseModel:
+        if isinstance(id, int):
+            with Session() as session:
+                result = session.query(self.__type_model).filter_by(id).first()
+            return result
+        else:
+            raise TypeError('Id must be an integer')
+
+    def delete(self, model: BaseModel) -> None:
+        with Session() as session:
+            session.delete(model)
+            session.commit()
