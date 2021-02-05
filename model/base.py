444ea389735004c735c879cb2d5554a7a73a3479
Clara Baumgarten <clara.odebrecht@olist.com>
2021-02-05 13:07:11 -0300

    Criação da base model

---
+++ model/base.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer
+
+Base = declarative_base()
+
+class BaseModel(Base):
+    __abstract__ = True
+    id_ = Column('id', Integer, primery_key=True)
